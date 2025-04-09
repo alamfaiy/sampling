fecd3571c0fa2a14f7fc33bbadd58cf6c450c582
alamfaiy <faiyza.alam@mail.utoronto.ca>
2025-04-09 17:40:06 -0400

    Made minor changes to code (1000 to 100, set seed)

--- 02_activities/assignments/whitby_covid_tracing.py
+++ 02_activities/assignments/whitby_covid_tracing.py
@@ -67,8 +67,11 @@ def simulate_event(m):
 
   return p_wedding_infections, p_wedding_traces
 
+# Set seed for reproducibility
+np.random.seed(36) 
+
 # Run the simulation 1000 times
-results = [simulate_event(m) for m in range(1000)]
+results = [simulate_event(m) for m in range(100)] # Modified to 100 from 1000
 props_df = pd.DataFrame(results, columns=["Infections", "Traces"])
 
 # Plotting the results
